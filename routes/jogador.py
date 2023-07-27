47b0e5a83540ca063e15e3383edbd6d64633505c
nfdeveloper <nfdeveloper04@gmail.com>
2023-07-26 21:59:48 -0300

    Implementando Lógica de Exclusão de ogadores

--- routes/jogador.py
+++ routes/jogador.py
@@ -46,3 +46,14 @@ async def atualiza_jogador(jogador_id, jogador:Jogador):
         )
     )
 
+    #Exclusão
+    @jogador_router.delete('/jogadores/{jogador_id}')
+    async def exclui_jogador(jogador_id):
+        return jogadorEntidade(
+           conexao.local.jogador.find_one_and_delete(
+            {
+               "_id": ObjectId(jogador_id)                
+            }
+           )
+        )
+
